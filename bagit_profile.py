9e9d64b58889b5fbacd1bb6b3090debcf3f58d4a
Konstantin Baierer <kba@users.noreply.github.com>
2018-11-13 15:17:02 +0100

    :art: make code pretty (#11)

--- bagit_profile.py
+++ bagit_profile.py
@@ -32,31 +32,28 @@ else:
 
 """
 
-# pylint: disable=logging-not-lazy, broad-except, import-error,
-# pylint: disable=import-error, no-name-in-module, multiple-imports
-# pylint: disable=unidiomatic-typecheck
-# pylint: disable=super-init-not-called
-
-import os
+from os.path import join, exists, isdir, isfile, split
 import sys
 import mimetypes
 import logging
-import optparse
 import json
 if sys.version_info > (3,):
-    from urllib.request import urlopen
+    from urllib.request import urlopen # pylint: no-name-in-module
 else:
-    from urllib import urlopen
+    from urllib import urlopen # pylint: disable=no-name-in-module
 
 # Define an exceptin class for use within this module.
 class ProfileValidationError(Exception):
+    # TODO: or just 'pass' instead of __init__ and __str__
     def __init__(self, value):
+        super(ProfileValidationError, self).__init__(value)
         self.value = value
     def __str__(self):
         return repr(self.value)
 
+
 # Define the Profile class.
-class Profile(object):
+class Profile(object): # pylint: disable=useless-object-inheritance
 
     def __init__(self, url, profile=None):
         self.url = url
@@ -84,8 +81,7 @@ class Profile(object):
             if sys.version_info > (3,):
                 profile = profile.decode('utf-8')
             profile = json.loads(profile)
-            profile = profile
-        except Exception as e:
+        except Exception as e: # pylint: disable=broad-except
             print("Cannot retrieve profile from %s: %s", self.url, e)
             logging.error("Cannot retrieve profile from %s: %s", self.url, e)
             # This is a fatal error.
@@ -145,8 +141,8 @@ class Profile(object):
     # Validate tags in self.profile['Bag-Info'].
     def validate_bag_info(self, bag):
         # First, check to see if bag-info.txt exists.
-        path_to_baginfotxt = os.path.join(bag.path, 'bag-info.txt')
-        if not os.path.exists(path_to_baginfotxt):
+        path_to_baginfotxt = join(bag.path, 'bag-info.txt')
+        if not exists(path_to_baginfotxt):
             self._fail("%s: bag-info.txt is not present." % bag)
         # Then check for the required 'BagIt-Profile-Identifier' tag and ensure it has the same value
         # as self.url.
@@ -170,7 +166,7 @@ class Profile(object):
             # If the tag is nonrepeatable, make sure it only exists once. We do this by checking to see if the value for the key is a list.
             if 'repeatable' in config and config['repeatable'] is False:
                 value = bag.info.get(tag)
-                if type(value) is list:
+                if isinstance(value, list):
                     self._fail("%s: Nonrepeatable tag '%s' occurs %s times in bag-info.txt." % (bag, tag, len(value)))
         return True
 
@@ -178,8 +174,8 @@ class Profile(object):
     # the manifest file is not present.
     def validate_manifests_required(self, bag):
         for manifest_type in self.profile['Manifests-Required']:
-            path_to_manifest = os.path.join(bag.path, 'manifest-' + manifest_type + '.txt')
-            if not os.path.exists(path_to_manifest):
+            path_to_manifest = join(bag.path, 'manifest-' + manifest_type + '.txt')
+            if not exists(path_to_manifest):
                 self._fail("%s: Required manifest type '%s' is not present in Bag." %  (bag, manifest_type))
         return True
 
@@ -190,8 +186,8 @@ class Profile(object):
         if 'Tag-Manifests-Required' not in self.profile:
             return True
         for tag_manifest_type in self.profile['Tag-Manifests-Required']:
-            path_to_tag_manifest = os.path.join(bag.path, 'tagmanifest-' + tag_manifest_type + '.txt')
-            if not os.path.exists(path_to_tag_manifest):
+            path_to_tag_manifest = join(bag.path, 'tagmanifest-' + tag_manifest_type + '.txt')
+            if not exists(path_to_tag_manifest):
                 self._fail("%s: Required tag manifest type '%s' is not present in Bag." % (bag, tag_manifest_type))
         return True
 
@@ -202,8 +198,8 @@ class Profile(object):
         if 'Tag-Files-Required' not in self.profile:
             return True
         for tag_file in self.profile['Tag-Files-Required']:
-            path_to_tag_file = os.path.join(bag.path, tag_file)
-            if not os.path.exists(path_to_tag_file):
+            path_to_tag_file = join(bag.path, tag_file)
+            if not exists(path_to_tag_file):
                 self._fail("%s: Required tag file '%s' is not present in Bag." % (bag, path_to_tag_file))
         return True
 
@@ -211,8 +207,8 @@ class Profile(object):
     # if the fetch.txt file exists. If it does, throw an exception.
     def validate_allow_fetch(self, bag):
         if self.profile['Allow-Fetch.txt'] is False:
-            path_to_fetchtxt = os.path.join(bag.path, 'fetch.txt')
-            if os.path.exists(path_to_fetchtxt):
+            path_to_fetchtxt = join(bag.path, 'fetch.txt')
+            if exists(path_to_fetchtxt):
                 self._fail("%s: Fetch.txt is present but is not allowed." % bag)
         return True
 
@@ -231,19 +227,17 @@ class Profile(object):
     # this method needs to be called before .validate().
     def validate_serialization(self, path_to_bag):
         # First, perform the two negative tests.
-        if not os.path.exists(path_to_bag):
+        if not exists(path_to_bag):
             raise IOError("Can't find file %s" % path_to_bag)
-        if self.profile['Serialization'] == 'required' and os.path.isdir(path_to_bag):
+        if self.profile['Serialization'] == 'required' and isdir(path_to_bag):
             self._fail("%s: Bag serialization is required but Bag is a directory." % path_to_bag)
-            #  return False
-        if self.profile['Serialization'] == 'forbidden' and os.path.isfile(path_to_bag):
+        if self.profile['Serialization'] == 'forbidden' and isfile(path_to_bag):
             self._fail("%s: Bag serialization is forbidden but Bag appears is a file." % path_to_bag)
-            #  return False
 
         # Then test to see whether the Bag is serialized (is a file) and whether the mimetype is one
         # of the allowed types.
-        if self.profile['Serialization'] == 'required' or self.profile['Serialization'] == 'optional' and os.path.isfile(path_to_bag):
-            _, bag_file = os.path.split(path_to_bag)
+        if self.profile['Serialization'] == 'required' or self.profile['Serialization'] == 'optional' and isfile(path_to_bag):
+            _, bag_file = split(path_to_bag)
             mtype = mimetypes.guess_type(bag_file)
             if mtype[0] not in self.profile['Accept-Serialization']:
                 self._fail("%s: Bag serialization is forbidden but Bag appears is a file." % path_to_bag)
@@ -255,6 +249,7 @@ def _main():
     # Command-line version.
     import time
     import bagit
+    import optparse # pylint: disable=deprecated-module
 
     class BagitProfileOptionParser(optparse.OptionParser):
         def __init__(self, *args, **opts):
@@ -273,7 +268,7 @@ def _main():
         else:
             level = logging.INFO
         if opts.log:
-            logFile = os.path.join(opts.log + '/logs', 'BagitProfile_' + time.strftime('%y_%m_%d') + '.log')
+            logFile = join(opts.log + '/logs', 'BagitProfile_' + time.strftime('%y_%m_%d') + '.log')
             logging.basicConfig(filename=logFile, level=level, format=log_format)
         if not opts.log:
             logging.basicConfig(filename='BagitProfile' + time.strftime('%y_%m_%d') + '.log', level=level, format=log_format)
